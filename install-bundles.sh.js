962d8f7c7545b36725a8d652684eeebb1b982350
Justin Russell <jus.russell@gmail.com>
2013-12-06 18:03:02 -0500

    fix: Update bundle if already installed

--- install-bundles.sh.js
+++ install-bundles.sh.js
@@ -1,13 +1,36 @@
 require('shelljs/global');
 
+var repoNameFromGitPath = function(gitPath) {
+  return gitPath.replace(/.*\//, '');
+};
+
 var installBundle = function(gitPath) {
-  gitPath = gitPath.trim().replace(/#.*/, '');
-  if(!gitPath) { return; }
   var repoName = gitPath.replace(/.*\//, '');
-  if(exec('git clone https://github.com/'+gitPath+'.git bundle/'+repoName).code != 0) {
+  if(exec('git clone https://github.com/'+gitPath+'.git bundle/'+repoName).code !== 0) {
     echo('Checkout failed for bundle '+gitPath);
     exit(1);
   }
 };
 
-cat('vimrc.bundles').split(/\r?\n/g).forEach(installBundle);
+var updateBundle = function(gitPath) {
+  var repoName = repoNameFromGitPath(gitPath);
+  pushd('bundle/' + repoName);
+  if(exec('git pull').code !== 0) {
+    echo('Update failed for bundle ' + gitPath);
+    exit(1);
+  }
+  popd();
+};
+
+var installOrUpdateBundle = function(gitPath) {
+  gitPath = gitPath.trim().replace(/#.*/, '');
+  if(!gitPath) { return; }
+  var repoName = repoNameFromGitPath(gitPath);
+  if(test('-d', 'bundle/' + repoName)) {
+    updateBundle(gitPath);
+  } else {
+    installBundle(gitPath);
+  }
+};
+
+cat('vimrc.bundles').split(/\r?\n/g).forEach(installOrUpdateBundle);
